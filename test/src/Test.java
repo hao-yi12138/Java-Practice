bcc24d2e881ba3a7bd94da4dbb11f7efffc5524a
hao-yi12138 <692180261@qq.com>
2020-04-14 10:26:58 +0800

    测试传代码

---
+++ test/src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: test
+ * @description: 实验
+ * @author: HAOYI
+ * @create: 2020-04-14-10:09
+ **/
+public class Test {
+    public static void main(String[] args) {
+    System.out.println("HELLO WORD");
+}
+}
